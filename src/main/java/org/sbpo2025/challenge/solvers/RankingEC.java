d89dd190f7a90ec79e426ae253ff23ea2ed9c41d
natanvek <50033520+natanvek@users.noreply.github.com>
2025-03-23 23:44:36 -0300

    RankingEC no anda rápido

---
+++ src/main/java/org/sbpo2025/challenge/solvers/RankingEC.java
@@ -0,0 +1,124 @@
+package org.sbpo2025.challenge.solvers; // Paquete correcto
+
+import org.sbpo2025.challenge.Heuristica;
+import org.sbpo2025.challenge.ChallengeSolution;
+
+import java.util.*;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+public class RankingEC extends Heuristica {
+    public RankingEC(List<Map<Integer, Integer>> orders, List<Map<Integer, Integer>> aislesh, int nItems,
+            int waveSizeLB,
+            int waveSizeUB) {
+        super(orders, aislesh, nItems, waveSizeLB, waveSizeUB);
+    }
+
+    public boolean insertCart(PriorityQueue<EfficientCart> queue, EfficientCart newCart, Integer regSize) {
+        if (queue.size() < regSize) {
+            queue.offer(newCart);
+            return true;
+        }
+
+        if (queue.peek().getValue() < newCart.getValue()) {
+            queue.poll();
+            queue.offer(newCart);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public ChallengeSolution solve(StopWatch stopWatch) {
+        int[] pesosAisle = new int[nAisles];
+
+        for (Order o : orders) {
+            if (o.size > waveSizeUB)
+                continue;
+            for (Aisle p : aisles) {
+                int ocupa = 0;
+
+                for (Map.Entry<Integer, Integer> entry : o.items.entrySet())
+                    ocupa += Math.min(p.items.getOrDefault(entry.getKey(), 0).intValue(), entry.getValue());
+
+                pesosAisle[p.id] += ocupa;
+            }
+        }
+
+        Arrays.sort(orders, (o1, o2) -> Integer.compare(o2.size, o1.size));
+        Arrays.sort(aisles, (a1, a2) -> Integer.compare(pesosAisle[a2.id], pesosAisle[a1.id]));
+
+        int[] idToPos = new int[nAisles];
+        for (int a = 0; a < nAisles; ++a)
+            idToPos[aisles[a].id] = a;
+
+        // Cart iniciarTope = pasada(as);
+
+        int tope = nAisles;
+        // long ti = stopWatch.getNanoTime();
+        // int iteraciones = 100;
+        // for(int it = 0; it < iteraciones; ++it){
+        // EfficientCart estimatingRegisterSize = EfficientCart.nullEfficientCart();
+        // for(Aisle a : aisles)
+        // estimatingRegisterSize = new EfficientCart(estimatingRegisterSize, a);
+
+        // fill(estimatingRegisterSize);
+        // }
+        // long delta_t = (stopWatch.getNanoTime() - ti) / iteraciones;
+
+        int registerSize = 5;
+        // int registerSize = (int) ((MAX_RUNTIME * 1e6) / (delta_t * tope * as));
+
+        System.out.println("register size: " + registerSize);
+
+        PriorityQueue<EfficientCart>[] rankings = (PriorityQueue<EfficientCart>[]) new PriorityQueue[nAisles + 1];
+        for (int i = 0; i <= nAisles; i++)
+            rankings[i] = new PriorityQueue<>(Comparator.comparingInt(EfficientCart::getCantItems));
+
+        insertCart(rankings[0], new EfficientCart(), registerSize);
+
+        // System.out.println("Pasillos: " + as);
+        for (Aisle p : aisles) {
+            // System.out.print("\rTopeActual: " + tope);
+
+            for (int r = tope - 1; r >= 0; --r) { // si vas de 0 a tope no funca
+                for (EfficientCart m : rankings[r]) {
+                    EfficientCart copia = new EfficientCart(m);
+                    copia.addAisle(p);
+                    // copia.setAvailable();
+                    
+                    // fill(copia);
+                    copia.fill();
+
+
+                    if (copia.cantItems >= waveSizeLB)
+                        tope = Math.min(tope, getTope(copia));
+
+                    insertCart(rankings[r + 1], copia, registerSize);
+                }
+            }
+        }
+
+        // System.out.println();
+
+        int guardo = 100;
+        EfficientCart rta = new EfficientCart();
+        PriorityQueue<EfficientCart> top10 = new PriorityQueue<>(Comparator.comparingDouble(EfficientCart::getValue));
+        for (int r = tope; r >= 0; --r)
+            for (EfficientCart m : rankings[r])
+                if (m.cantItems >= waveSizeLB) {
+                    rta = updateAnswer(rta, m);
+                    insertCart(top10, m, guardo);
+                }
+
+        System.out.print("[");
+
+        for (Integer a : rta.my_aisles)
+            System.out.print(idToPos[a] + ", ");
+
+        System.out.println("]");
+
+        return getSolution(rta);
+    }
+}
